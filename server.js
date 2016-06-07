a9a4996f7efbed0c8d030c0e3d64ddc1bcdf7ba5
Dale <drcorns@hotmail.com>
2016-06-07 15:22:12 -0700

    add database connection

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
  */
 'use strict';
 let express = require('express');
+let corngoose = require('corngoose');
 let app = express();
 let server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 let server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -17,4 +18,5 @@ let server = app.listen(server_port, server_ip_address, function(){
   let host = server.address().address;
   let port = server.address().port;
   console.log('Server listening on ' + host + ', port: ' + port);
+  corngoose.startDB('drc');
 });
