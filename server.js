b9d57cbf4f3ed8c8b3bf483543ac0b80b8223150
Dale <drcorns@hotmail.com>
2016-06-13 23:10:42 -0700

    add index page

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
  * Copyright © 2016 Dale Corns
  */
 'use strict';
-//comment to force push
 let express = require('express');
 let firebase = require('firebase');
 let corngoose = require('corngoose');
@@ -12,18 +11,12 @@ let app = express();
 let server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 let server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 corngoose.startDB('drc');
-setTimeout(function(){
-  corngoose.getCollection('users', function(err, data){
-    if(err) console.dir(err);
-    console.dir(data);
-  });
-  corngoose.showDbPath();
-}, 1000);
 
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello!</h1>');
+  res.sendFile('index.html');
 });
 
 let server = app.listen(server_port, server_ip_address, function(){
